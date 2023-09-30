c745278e0dfc7fc1b7c30c5a385fa029b87d5caa
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-09-30 11:32:18 -0300

    trabalho daniel

---
+++ Migrations/20230930143039_asd.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CadastroDeFornecedores.Migrations
+{
+    /// <inheritdoc />
+    public partial class asd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CNPJ = table.Column<string>(type: "text", nullable: false),
+                    RazaoSocial = table.Column<string>(type: "text", nullable: true),
+                    NomeFantasia = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Telefone = table.Column<string>(type: "text", nullable: true),
+                    NomePessoaContato = table.Column<string>(type: "text", nullable: false),
+                    Rua = table.Column<string>(type: "text", nullable: true),
+                    Numero = table.Column<string>(type: "text", nullable: true),
+                    Complemento = table.Column<string>(type: "text", nullable: true),
+                    Bairro = table.Column<string>(type: "text", nullable: true),
+                    Estado = table.Column<string>(type: "text", nullable: true),
+                    Municipio = table.Column<string>(type: "text", nullable: true),
+                    CEP = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedor", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fornecedor");
+        }
+    }
+}
